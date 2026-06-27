4a66d7b815df11814e1bc1b707ac6e45791665ba
Hridyesh-0205 <hridyeshs5@gmail.com>
2026-06-27 14:11:45 +0530

    app.js

--- app.js
+++ app.js
@@ -1,87 +1,76 @@
-let userScore=0;
-let compScore=0;
-const choices=document.querySelectorAll(".choice");
-const msg=document.querySelector("#msg");
+let userScore = 0;
+let compScore = 0;
 
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
-const userScorepara=document.querySelector("#user-score");
-const compScorepara=document.querySelector("#comp-score");
+const userScorepara = document.querySelector("#user-score");
+const compScorepara = document.querySelector("#comp-score");
 
-const genCompChoice=()=>{
-    const options=["rock","paper","scissors"];
-    const ranIdx=Math.floor(Math.random()*3);
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const ranIdx = Math.floor(Math.random() * 3);
     return options[ranIdx];
+};
 
-}
+const drawgame = () => {
+    msg.innerText = "Game was Draw!";
+    msg.style.backgroundColor = "grey";
+};
 
-const drawgame=()=>{
-
-     msg.innerText="game get draw";
-     msg.style.backgroundColor="grey";
-}
-
-const showwinner=(userwin,userChoice,compChoice)=>{
-    if(userwin){
+const showwinner = (userwin, userChoice, compChoice) => {
+    if (userwin) {
         userScore++;
-        userScorepara.innerText=userScore;
-        console.log("you win");
-        msg.innerText=`you win  yours ${userChoice}beats ${compChoice}`;
-        msg.style.backgroundColor="green";
-    }
-     else{
+        userScorepara.innerText = userScore;
+
+        msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
         compScore++;
-        compScorepara.innerText=compScore;
-     
-        msg.innerText=`you lose ${compChoice}beats yours ${userChoice}`;
-        msg.style.backgroundColor="red";
+        compScorepara.innerText = compScore;
+
+        msg.innerText = `You Lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
     }
+};
+
+const playgame = (userChoice) => {
+
+    const compChoice = genCompChoice();
+
+    console.log("User Choice:", userChoice);
+    console.log("Computer Choice:", compChoice);
+
+    if (userChoice === compChoice) {
+        drawgame();
+    } else {
+
+        let userwin;
+
+        if (userChoice === "rock") {
+            // Rock beats Scissors
+            userwin = compChoice === "scissors";
+        }
+
+        else if (userChoice === "paper") {
+            // Paper beats Rock
+            userwin = compChoice === "rock";
+        }
+
+        else if (userChoice === "scissors") {
+            // Scissors beats Paper
+            userwin = compChoice === "paper";
+        }
+
+        showwinner(userwin, userChoice, compChoice);
     }
+};
 
-
-const playgame=(userChoice)=>{
-    
-    
-   const compChoice=genCompChoice();
-  
-
-
-   if(userChoice===compChoice){
-    drawgame()
-   }
-   else{
-    let userwin=true;
-    if(userChoice==="rock"){
-        //scissors,paper
-        userwin= compChoice==="paper"?false:true;
-
-    } 
-     else if( userChoice==="paper")
-    {
-       userwin = compChoice === "paper" ? false : true;
-    }
-     else( userChoice==="scissors")
-    {
-        userwin=compChoice==="rock"?false:true;
-    }
-
-    showwinner(userwin,userChoice,compChoice);
-    }
-   }
-    
-
-
-
-choices.forEach((choice )=> {
-    choice.addEventListener("click",()=>{
-        const userChoice=choice.getAttribute("id");
-       playgame(userChoice);
-
-
-
-    })
-
-
-    
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playgame(userChoice);
+    });
 });
 
 const resetBtn = document.querySelector("#reset-btn");
@@ -95,7 +84,6 @@ const resetGame = () => {
 
     msg.innerText = "Play your move";
     msg.style.backgroundColor = "#081b31";
-    msg.style.fontSize = "2rem";
 };
 
 resetBtn.addEventListener("click", resetGame);
